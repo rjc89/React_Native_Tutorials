9b834cfc3aab67ae68ab68bafccf7cba77afdde1
Robert <rjconstable1989@gmail.com>
2020-07-20 18:43:35 +0100

    more updates

--- App.js
+++ App.js
@@ -1,23 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import logo from './assets/favicon.png'; 
+import logo from './assets/favicon.png';
+ 
 
 export default function App() {
   return (
+    
+      
     <View style={styles.container}>
       <Image source={logo} style={{ width: 50, height: 50 }} />
       <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo}/> 
       <Text style={styles.instructions}> 
       Open up App.js to start working on your app!
       </Text>
+
+      <View style = {styles.header}>
+      <Text>MEGADOODOO</Text>
       <TouchableOpacity
         onPress={() => alert('Hello, world!')}
-        style={{ backgroundColor: 'blue' }}>
-        <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>
+        style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
       </TouchableOpacity>
       <StatusBar style="auto" />
-    </View>
+    </View></View>
   );
 }
 
@@ -37,6 +43,19 @@ const styles = StyleSheet.create({
     color: '#888',
     fontSize: 18,
     marginHorizontal: 15,
+  },
+  button: {
+    backgroundColor: "black",
+    padding: 20,
+    borderRadius: 8,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  },
+  header: {
+    backgroundColor:'pink',
+    padding: 20,
   }, 
 });
 
