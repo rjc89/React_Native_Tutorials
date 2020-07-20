ceb5c00f5eb6c5a2b14f69dcb69aef2c294c6587
Robert <rjconstable1989@gmail.com>
2020-07-20 17:52:22 +0100

    commit 2

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import logo from './assets/favicon.png'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} style={{ width: 50, height: 50 }} />
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo}/> 
+      <Text style={styles.instructions}> 
+      Open up App.js to start working on your app!
+      </Text>
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={{ backgroundColor: 'blue' }}>
+        <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +24,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#561dbf',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+  }, 
 });
+
