e80701a676d178e8f66154b2396535dda13fbfe3
Robert <rjconstable1989@gmail.com>
2020-07-21 22:38:35 +0100

    building the review app

--- basic/App.js
+++ basic/App.js
@@ -1,27 +1,31 @@
 import React, {useState} from 'react';
+// import { StyleSheet, Text, View} from 'react-native';
 import * as Font from 'expo-font';
 import Home from './screens/home';
-import {AppLoading} from 'expo';
+import { AppLoading } from 'expo';
 import Navigator from './routes/homeStack';
 
 const getFonts = () => Font.loadAsync({
   'nunito-regular' : require('./assets/fonts/Nunito-Regular.ttf'),
-  'nunito-bold': require('./assets/fonts/Nunito-Bold.tff'),
-});
+  'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+  });
+
 
 export default function App(){
   const [fontsLoaded, setFontsLoaded] = useState(false);
 
   if(fontsLoaded){
+
     return (
-      <Navigator />
+    <Navigator />
     );
   } else {
-    return (
+    return(
       <AppLoading
-      startAsync={getFonts}
-      onFinish={() => setFontsLoaded(true)}
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
       />
     )
+
   }
 }
